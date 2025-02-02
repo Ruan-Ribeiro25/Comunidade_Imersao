1f2c94001d096e0fb57ecf4e468218507090708f
Ruan Ribeiro <ruanribeiro2501@gmail.com>
2025-02-02 00:09:16 -0300

    Ajustes no Menu Toggle e CSS

--- script1.js
+++ script1.js
@@ -1,10 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var menuToggle = document.getElementById("menu-toggle");
-    var menu = document.getElementById('menu');
-    var menuIcon = document.getElementById("menu-icon");
-
-    menuToggle.addEventListener("click", function () {
-        menu.classList.toggle('open'); // Adiciona ou remove a classe 'open' no menu
-        menuIcon.textContent = menu.classList.contains('open') ? "✕" : "☰";
-    });
+    // Código limpo sem referências ao ícone e ao menu-toggle
 });
